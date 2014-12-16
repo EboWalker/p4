afaea26edbfc127a95aed3d5ee0ad757ea254a2c
Chad Kirchner <chadkirchner@gmail.com>
2014-12-15 23:50:37 -0500

    database file

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => 'localhost',
 			'database'  => 'taskapp',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'D0zALF9tSx',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
